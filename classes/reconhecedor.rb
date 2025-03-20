61cb06a302b97e8adfcf8be3c5b611e031ddaa33
thiagopls1 <thiagopls1@hotmail.com>
2025-03-20 16:11:35 -0300

    Add recognition tests

--- classes/reconhecedor.rb
+++ classes/reconhecedor.rb
@@ -3,34 +3,34 @@ class Reconhecedor
     @texto = texto
   end
 
-  def reconhecer_horario()
-    regex = /((\d{1,2}) e (\d{1,2}))|((\d{2})[:\s](\d{2}))|((\d{1,2})( horas?)?)/
+  def horario()
+    regex = /(\d{1,2} e \d{1,2})|(\d{1,2}[:\s]\d{2})|(\d{1,2}( horas?))/
     resultRaw = regex.match(@texto).to_s
     resultDigits = resultRaw.scan(/\d{1,2}/)
     result = "#{resultDigits[0] || "00"}:#{resultDigits[1] || "00"}"
     return result
   end
 
-  def reconhecer_tarefa()
+  def tarefa()
     # ???
   end
 
-  def reconhecer_url()
+  def url()
     regex = /http(s)?:\/\/(www\.)?[A-z\-]+(\.[A-z\-]+)*(:[0-9]+)?(\/\w+)*\/?(\?(\w+=\w+)+(&\w+=\w+)*)?/
     return @texto.match(regex)
   end
 
-  def reconhecer_email()
+  def email()
     regex = /[A-z0-9]+[A-z0-9\.-]*@[A-z]+[A-z0-9\.-]*/
     return @texto.match(regex)
   end
 
-  def reconhecer_tag()
-    regex = /#\w+/
-    return @texto.match(regex)
+  def tags()
+    regex = /#[\w-]+/
+  return @texto.scan(regex)
   end
 
-  def reconhecer_pessoa()
+  def pessoa()
     regex = /[A-Z]{1}[a-zà-ÿ]+/
     return @texto.match(regex)
   end
--- main.rb
+++ main.rb
@@ -1,9 +1,12 @@
 require './classes/tarefa.rb'
 require './classes/reconhecedor.rb'
 
-texto = "Marque uma agenda às 10:30"
+texto = "Me lembre de acessar o site https://google.com.br com a conta thiagopls1@hotmail.com às 10:30 #trabalho #importante"
 reconhecedor = Reconhecedor.new(texto)
-puts reconhecedor.reconhecer_horario()
+puts reconhecedor.horario()
+puts reconhecedor.url()
+puts reconhecedor.email()
+puts reconhecedor.tags()
 # nova_tarefa = Tarefa.new
 # puts "Nova tarefa criada"
 # puts nova_tarefa
