3e42821134668b5a52dcfb4a44c4c182e5adcb46
thiagopls1 <thiagopls1@hotmail.com>
2025-03-21 13:29:26 -0300

    Add `to_s`

--- classes/tarefa.rb
+++ classes/tarefa.rb
@@ -1,13 +1,32 @@
 # Por enquanto, usaremos todos os parâmetros como opcionais
 
 class Tarefa
-  def initialize(tag=nil, horario=nil, dia=nil, acao=nil, url=nil, email=nil, pessoa=nil)
-    @tag = tag
-    @dia = dia
-    @horario = horario
-    @acao = acao
-    @url = url
-    @email = email
-    @pessoa = pessoa
+  def initialize(texto, tags=nil, horario=nil, data=nil, acao=nil, url=nil, email=nil, pessoa=nil)
+    @texto = texto        # Texto inserido pelo usuário
+    @tags = tags          # Tags reconhecidas no texto
+    @data = data          # Data reconhecida no texto
+    @horario = horario    # Horario reconhecido no texto
+    @acao = acao          # Ações que nós escolhemos reconhecidas no texto *
+    @url = url            # Url reconhecida no texto *
+    @email = email        # E-mail reconhecido no texto *
+    @pessoa = pessoa      # Pessoa reconhecida no texto *
+
+    # Os marcados com * precisa de algum ajuste de modelagem:
+    # - É necessário reconhecer vários e-mails?
+    # - É necessário reconhecer várias pessoas?
+    # - É necessário reconhecer várias URLs?
+    # - Quais ações vamos escolher? (Comentado no reconhecedor.rb)
+  end
+
+  def to_s
+    return \
+    "Texto: #{@texto}\n" +
+    "Tags: #{@tags.join(" ")}\n" +
+    "Data: #{@data.strftime('%d/%m/%Y')}\n" +
+    "Horario: #{@horario}\n" +
+    "Email: #{@email}\n" +
+    "Url: #{@url}\n" +
+    "Pessoa: #{@pessoa}\n" +
+    "Ação: #{@acao}"
   end
 end
